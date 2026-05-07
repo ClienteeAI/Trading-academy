80eba20ba9d98245c0156175cb85c7fdd5a01e75
ClienteeAI <petr@clientee.co>
2026-05-07 11:33:13 +0200

    chore: remove scratch test

--- scratch_test.js
+++
@@ -1,6 +0,0 @@
-import { linkifyContent } from './src/utils/termLinker.js';
-import { dictionaryData } from './src/data/dictionaryData.js';
-
-const testContent = '<p>Likvidita je velmi důležitá pro trading. Nízká likvidita způsobuje volatilitu.</p>';
-const result = linkifyContent(testContent, dictionaryData, 'cs');
-console.log('RESULT:', result);
